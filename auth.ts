58067bfe2ba63371c65845b593c0c9691c199f2d
Danny <d.markov94@gmail.com>
2024-04-18 18:46:50 +0300

    Reflow project changes

--- auth.ts
+++ auth.ts
@@ -2,9 +2,9 @@ import "server-only";
 import { ReflowAuth } from "@reflowhq/auth-next";
 
 export default function getAuth(): ReflowAuth {
-  const { SESSION_SECRET, REFLOW_STORE_ID } = process.env;
+  const { SESSION_SECRET, REFLOW_PROJECT_ID } = process.env;
   const config = {
-    storeID: Number(REFLOW_STORE_ID),
+    projectID: Number(REFLOW_PROJECT_ID),
     secret: String(SESSION_SECRET),
   };
   return new ReflowAuth(config);
